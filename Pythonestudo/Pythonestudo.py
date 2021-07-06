0307e1bef2559abf6b746b51bd7621211a390304
gabri <gabri@DESKTOP-NN5KSSD>
2021-07-06 10:25:14 -0300

    Adicionar arquivos de projeto.

---
+++ Pythonestudo/Pythonestudo.py
@@ -0,0 +1,15 @@
+a = 3
+b = 5
+soma = a + b
+quadrado = soma ** 2
+
+print(soma)
+print(quadrado)
+
+if soma == 8:
+      print('A soma é oito!')
+else:
+      print('A soma não é 8!')
+
+for n in range(5):
+      print(n)
